1c2def580ee8bc95dd63de0247df886f04667903
yangjie18 <yangjie18@baidu.com>
2018-01-18 14:20:08 +0800

    修复BuildConfig冲突问题

--- android/src/main/java/com/baidu/mobstat/RNBaiduMobStatPackage.java
+++ android/src/main/java/com/baidu/mobstat/RNBaiduMobStatPackage.java
@@ -28,7 +28,6 @@ public class RNBaiduMobStatPackage implements ReactPackage  {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
