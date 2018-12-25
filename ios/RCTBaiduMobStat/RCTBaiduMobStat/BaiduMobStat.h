608a36b1a5c46a2569dc8be01cd9d6d50328ba28
lidongdong01 <lidongdong01@baidu.com>
2018-12-25 17:24:29 +0800

    update version

--- ios/RCTBaiduMobStat/RCTBaiduMobStat/BaiduMobStat.h
+++ ios/RCTBaiduMobStat/RCTBaiduMobStat/BaiduMobStat.h
@@ -11,6 +11,7 @@
 #ifdef __IPHONE_8_0
 #import <WebKit/WebKit.h>
 #endif
+
 @class UIViewController;
 
 /**
@@ -18,18 +19,46 @@
  */
 typedef enum _BaiduMobStatLogStrategy {
     BaiduMobStatLogStrategyAppLaunch = 0,   //每次程序启动时发送（默认策略，推荐使用!）
-    BaiduMobStatLogStrategyDay = 1,         //每天的程序第一次进入启动
-    BaiduMobStatLogStrategyCustom = 2,      //根据设定的时间间隔发送
 } BaiduMobStatLogStrategy;
 
 /**
+ 推送平台定义
+ */
+typedef enum _BaiduMobStatPushPlatform{
+    BaiduMobStatPushPlatformBaiduCloud = 0, //百度云推送平台
+    BaiduMobStatPushPlatformJiGuang = 1,    //极光推送平台
+    BaiduMobStatPushPlatformGeTui = 2,      //个推推送平台
+    BaiduMobStatPushPlatformUmeng = 5,      //友盟推送平台
+    BaiduMobStatPushPlatformXinGe = 6,      //信鸽推送平台
+    BaiduMobStatPushPlatformAliYun = 7,     //阿里云推送平台
+} BaiduMobStatPushPlatform;
+
+/**
+ 信息流监控策略
+ */
+typedef enum _BaiduMobStatFeedTrackStrategy {
+    BaiduMobStatFeedTrackStrategyAll = 0,       //全部监控
+    BaiduMobStatFeedTrackStrategySingle = 1,    //单个监控，需要监控的tableviw需要设置enableListTrack属性为YES
+    BaiduMobStatFeedTrackStrategyNone = 2,      //关闭feed监控，不监控任何列表
+} BaiduMobStatFeedTrackStrategy;
+
+/**
  百度移动应用统计接口
- 当前版本 4.6.1
+ 当前版本 4.9.5_18
  */
 @interface BaiduMobStat : NSObject
 /**
- 以下property属性，均为可选设置
+ 以下property属性，均为可选设置。
+ 如需设置，必须在startWithAppId:方法前调用才可生效。
  */
+
+/**
+ 设置用户自定义的用户识别id，在startWithAppId之前调用
+ 设置一次UserId后，用户被永久标记。传入新的userId将替换老的userId。
+ 传入nil或空字符串@""，可清空标记。
+ 自定义规则的用户识别id（可以使登录用户账号、手机号等），长度限制256字节
+ */
+@property (nonatomic, copy) NSString *userId;
 
 /**
  设置app的版本号
@@ -70,14 +99,6 @@ typedef enum _BaiduMobStatLogStrategy {
 @property (nonatomic) BaiduMobStatLogStrategy logStrategy;
 
 /**
- 设置日志发送时间间隔
- 当logStrategy设置为BaiduMobStatLogStrategyCustom时生效
- 单位为小时，有效值为1~24
- 默认值为 1
- */
-@property (nonatomic) int logSendInterval;
-
-/**
  设置是否打印SDK中的日志，用于调试
  默认值 NO
  */
@@ -90,12 +111,57 @@ typedef enum _BaiduMobStatLogStrategy {
  */
 @property (nonatomic, copy) NSString *adid;
 
-
 /**
  设置是否获取GPS信息（V4.1.1新增）
  默认为YES，获取GPS信息
  */
 @property (nonatomic, assign) BOOL enableGps;
+
+/**
+ 设置是否允许监控webview（V4.6.7新增）
+ 默认为YES
+ */
+@property (nonatomic, assign) BOOL enableWebViewAutoTrack;
+
+/**
+ 当enableWebViewAutoTrack属性设置为NO时，该接口无效，可忽略。
+ 当多个webview的delegate类存在继承关系时，为了避免因调用super方法而导致的死循环风险，可以设置是否只统计子类Delegate（V4.9.5新增）
+ 备注：在webview的delegate方法中没有调用super方法的情况下，不受影响。此时为了保证数据完整，不建议设置为YES。
+ 默认为NO
+ */
+@property (nonatomic, assign) BOOL onlyTrackSubClassOfWebDelegate;
+
+/**
+ 设置UIViewController是否监控（V4.6.7新增）
+ 默认为YES
+ */
+@property (nonatomic, assign) BOOL enableViewControllerAutoTrack;
+
+/**
+ 设置控件点击event事件是否监控（V4.8.3新增）
+ 默认为YES
+ */
+@property (nonatomic, assign) BOOL enableEventAutoTrack;
+
+/**
+ 设置是否允许推送文案的采集，用于启动来源分析（V4.9.5新增）
+ 默认为YES
+ */
+@property (nonatomic, assign) BOOL enableGetPushContent;
+
+/**
+ 设置TableView监控策略（V4.8.0新增）
+ 默认为BaiduMobStatFeedTrackStrategyAll，全部监控
+ 设置为BaiduMobStatFeedTrackStrategySingle单个监控，则要监控的tableview需要设置enableListTrack属性为YES
+ 设置为BaiduMobStatFeedTrackStrategyNone，则不监控任何列表
+ */
+@property (nonatomic, assign) BaiduMobStatFeedTrackStrategy feedTrackStrategy;
+
+/**
+ 设置是否使用启动来源分析功能（V4.9.5新增）
+ 默认为YES
+ */
+@property (nonatomic, assign) BOOL enableLaunchRefererTrack;
 
 /**
  获取统计对象的实例
@@ -145,7 +211,6 @@ typedef enum _BaiduMobStatLogStrategy {
  */
 - (void)eventEnd:(NSString *)eventId eventLabel:(NSString *)eventLabel;
 
-
 /**
  记录一次事件的点击，eventId和对应的attribute的key请在网站上创建，未创建的evenId和key将无法统计。
  
@@ -174,7 +239,6 @@ typedef enum _BaiduMobStatLogStrategy {
  */
 - (void)eventEnd:(NSString *)eventId eventLabel:(NSString *)eventLabel attributes:(NSDictionary *) attributes;
 
-
 /**
  记录某个页面访问的开始，请参见Example程序，在合适的位置调用。
  建议在ViewController的viewDidAppear函数中调用
@@ -183,7 +247,6 @@ typedef enum _BaiduMobStatLogStrategy {
  */
 - (void)pageviewStartWithName:(NSString *)name;
 
-
 /**
  记录某个页面访问的结束，与pageviewStartWithName配对使用，请参见Example程序，在合适的位置调用。
  建议在ViewController的viewDidDisappear函数中调用
@@ -191,7 +254,6 @@ typedef enum _BaiduMobStatLogStrategy {
  @param name 页面名称
  */
 - (void)pageviewEndWithName:(NSString *)name;
-
 
 /**
  记录UIWebView中的行为（需要在网页的JS代码中进行相应配置，详见文档与Demo程序）
@@ -213,6 +275,21 @@ typedef enum _BaiduMobStatLogStrategy {
  @param body WKScriptMessage的body 只接受NSDictionary类型
  */
 - (void)didReceiveScriptMessage:(NSString*)name body:(NSDictionary *)body;
+
+/**
+ 主动上传的Exception信息记录
+ 
+ @param exception 自己捕获的，需要上传的exception
+ */
+- (void)recordException:(NSException *)exception;
+
+/**
+ 主动上传的NSError信息记录
+ 
+ @param error 自己捕获的，需要上传的error
+ */
+- (void)recordError:(NSError *)error;
+
 /**
  获取cuid的值
  返回SDK生成的cuid
@@ -221,8 +298,22 @@ typedef enum _BaiduMobStatLogStrategy {
  */
 - (NSString *)getDeviceCuid;
 
-@end
+/**
+ 获取设备的测试ID，同时有Log打印
+ 返回SDK生成的设备测试ID
+ 
+ @return 设备测试ID
+ */
+- (NSString *)getTestDeviceId;
 
+/**
+ 上传第三方Push平台的Id，pushId长度限制1024字节。设置为nil或者空字符串，则清空对应平台的pushId
+ 
+ @prama pushId 从第三方Push SDK接口中获取的pushId
+ @prama platform 指定Push平台类型，详见枚举声明
+ */
+- (void)setPushId:(NSString *)pushId platform:(BaiduMobStatPushPlatform)platform;
+@end
 
 /**
  Category 声明
@@ -236,20 +327,16 @@ typedef enum _BaiduMobStatLogStrategy {
 /**
  手动设置自动收集的页面名称
  */
-@property (nonatomic, strong) NSString *titleForBaiduMobStat;
+@property (nonatomic, copy) NSString *titleForBaiduMobStat;
 
-/**
- 请勿设置此变量值！！！
- 此变量由SDK内部控制，页面开始时间，供SDK内部使用
- */
-@property (nonatomic, strong) NSNumber *pageStartTime;
+// SDK内部属性，不要设置
+#define UIViewControllerInternalDefine @property (nonatomic, strong) UIScrollView *contentScorllView;@property (nonatomic, strong) NSNumber *pageStartTime;@property (nonatomic, copy) NSString *userHandle;@property (nonatomic, copy) NSString *eventTitle;@property (nonatomic, strong) NSDictionary *mtjDictionary;@property (nonatomic, strong) NSNumber *titleCount;
+UIViewControllerInternalDefine
 
 @end
 
-
 /**
  百度移动统计UIWindow Category
- 
  */
 @interface UIWindow (BaiduMobStatWindow)
 @end
@@ -264,6 +351,10 @@ typedef enum _BaiduMobStatLogStrategy {
  对应的key需要在网站上创建，注意：value只接受NSString
  */
 @property (nonatomic, strong) NSDictionary *attributesForBaiduMobStat;
+
+// SDK内部属性，不要设置
+#define UIViewInternalDefine @property (nonatomic, copy) NSString *hasTable;
+UIViewInternalDefine
 @end
 
 /**
@@ -288,3 +379,63 @@ typedef enum _BaiduMobStatLogStrategy {
 
 @end
 #endif
+
+@interface UIScrollView (BaiduMobStatScrollView)
+
+@end
+
+/**
+ 百度移动统计UITableView Category
+ */
+@interface UITableView (BaiduMobStatTableView)
+
+/**
+ 设置TableView的栏目名称，为MTJ分析信息流报表中的”栏目名称“,长度限制256字节，超出截断
+ 如果不设置，则默认采用tableview所在的Controller title作为“栏目名称”
+ 建议在TableView加载前设置。
+ */
+@property (nonatomic, copy) NSString *listName;
+
+/**
+ 设置单个TableView是否需要被监控
+ 默认情况下，sdk监控全局TableView，不需要设置该接口
+ 当设置了全局关闭之后，可以设置单个tableview打开指定的TableView监控
+ */
+@property (nonatomic, assign) BOOL enableListTrack;
+
+// SDK内部属性，不要设置
+#define UITableViewInternalDefine @property (atomic, copy) NSString *viewLevel;@property (atomic, copy) NSString *viewStatu;@property (atomic, strong) NSArray *lastCellArr;@property (atomic, copy) NSString *autoName;@property (atomic, strong) NSNumber *lastTime;@property (atomic, strong) NSNumber *startTime;@property (atomic, strong) NSNumber *endTime;
+UITableViewInternalDefine
+@end
+
+@interface UITableViewCell (BaiduMobStatTableViewCell)
+
+/**
+ 设置Cell的信息Title，为MTJ分析信息流报表中的”信息标题“,长度限制256字节，超出截断
+ 如果不设置，则SDK会自动识别一个Title
+ 建议在cell加载前设置。
+ */
+@property (nonatomic, copy) NSString *contentTitle;
+
+/**
+ 设置Cell的信息ID，是用户自身系统中的内容id,长度限制256字节，超出截断
+ 如果不设置，则id为空
+ 建议在cell加载前设置。
+ */
+@property (nonatomic, copy) NSString *contentId;
+
+@end
+
+@interface UIApplication (BaiduMobStatApplication)
+@end
+
+void import_BaiduMobStatViewController ();
+void import_BaiduMobStatWindow ();
+void import_BaiduMobStatView ();
+void import_BaiduMobStatWebView ();
+void import_BaiduMobStatWKWebView ();
+void import_BaiduMobStatNSInvocation ();
+void import_BaiduMobStatScrollView ();
+void import_BaiduMobStatTableView ();
+void import_BaiduMobStatTableViewCell ();
+void import_BaiduMobStatApplication ();
